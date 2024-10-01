7d3797bd87fa64aad7cbf3e63bb20e12099b23c3
powillmott <powrunner@gmail.com>
2024-09-30 21:32:55 -0600

    Implemented getTeamTurn and setTeamTurn

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -10,15 +10,16 @@ import java.util.Collection;
  */
 public class ChessGame {
 
+    private final TeamColor team;
     public ChessGame() {
-
+        this.team = TeamColor.WHITE;
     }
 
     /**
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return team;
     }
 
     /**
@@ -27,7 +28,12 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        if(team == TeamColor.BLACK) {
+            team = TeamColor.WHITE;
+        }
+        if(team == TeamColor.WHITE) {
+            team = TeamColor.BLACK;
+        }
     }
 
     /**
