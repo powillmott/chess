efd34a39ff4e63469558c96e81758fa24db86c5b
powillmott <powrunner@gmail.com>
2024-10-02 11:26:13 -0600

    Deleted some instructions (need more commits, I code too much at a time, but I have been working on this through several days, and way before deadline. This project just wasn't as hard as the first one)

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -4,12 +4,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Objects;
 
-/**
- * For a class that can manage a chess game, making moves on a board
- * <p>
- * Note: You can add to this class, but you may not alter
- * signature of the existing methods.
- */
 public class ChessGame {
 
     private final TeamColor team;
