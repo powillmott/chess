a47f07a6fbb0161b162c6ecf32198c269e4927de
powillmott <powrunner@gmail.com>
2024-10-23 19:50:29 -0600

    one function call was too long I guess. So it got indented

--- shared/src/main/java/chess/RookCalculator.java
+++ shared/src/main/java/chess/RookCalculator.java
@@ -11,26 +11,27 @@ public class RookCalculator implements MovementRules {
         return getRookChessMoves(board, position, position, color, temp);
     }
 
-    public ArrayList<ChessMove> getRookChessMoves(ChessBoard board, ChessPosition position, ChessPosition pos, ChessGame.TeamColor color, ArrayList<ChessMove> temp) {
+    public ArrayList<ChessMove> getRookChessMoves(ChessBoard board, ChessPosition pos1, ChessPosition pos2,
+                                                  ChessGame.TeamColor color, ArrayList<ChessMove> temp) {
         QueenCalculator queenCalc = new QueenCalculator();
-        while (queenCalc.moveCheck(pos, board, -1, 0, color)) {
-            pos = new ChessPosition(pos.getRow() - 1, pos.getColumn());
-            temp.add(new ChessMove(position, pos, null));
+        while (queenCalc.moveCheck(pos2, board, -1, 0, color)) {
+            pos2 = new ChessPosition(pos2.getRow() - 1, pos2.getColumn());
+            temp.add(new ChessMove(pos1, pos2, null));
         }
-        pos = position;
-        while (queenCalc.moveCheck(pos, board, 1, 0, color)) {
-            pos = new ChessPosition(pos.getRow() + 1, pos.getColumn());
-            temp.add(new ChessMove(position, pos, null));
+        pos2 = pos1;
+        while (queenCalc.moveCheck(pos2, board, 1, 0, color)) {
+            pos2 = new ChessPosition(pos2.getRow() + 1, pos2.getColumn());
+            temp.add(new ChessMove(pos1, pos2, null));
         }
-        pos = position;
-        while (queenCalc.moveCheck(pos, board, 0, -1, color)) {
-            pos = new ChessPosition(pos.getRow(), pos.getColumn() - 1);
-            temp.add(new ChessMove(position, pos, null));
+        pos2 = pos1;
+        while (queenCalc.moveCheck(pos2, board, 0, -1, color)) {
+            pos2 = new ChessPosition(pos2.getRow(), pos2.getColumn() - 1);
+            temp.add(new ChessMove(pos1, pos2, null));
         }
-        pos = position;
-        while (queenCalc.moveCheck(pos, board, 0, 1, color)) {
-            pos = new ChessPosition(pos.getRow(), pos.getColumn() + 1);
-            temp.add(new ChessMove(position, pos, null));
+        pos2 = pos1;
+        while (queenCalc.moveCheck(pos2, board, 0, 1, color)) {
+            pos2 = new ChessPosition(pos2.getRow(), pos2.getColumn() + 1);
+            temp.add(new ChessMove(pos1, pos2, null));
         }
 
         return temp;
